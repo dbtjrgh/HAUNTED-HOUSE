76e3e63e4852e7f9ee4471d7a3a5c61d0deb82ed
유석호 <dbtjrgh0522@naver.com>
2024-08-28 06:23:01 +0900

    08.28 타이틀 씬 및 로딩 씬 구현 완료
    예정
    로비씬 작업 진행
    해당 씬에서 플레이어가 게임시작 UI쪽으로 접근해서 포톤을 이용해 참가시키는 형식으로 적용 예정
    참가가 되면 해당 씬에서  플레이어들이 모여서 시작하는 식

--- Assets/_Seokho/Dark - Complete Horror UI/Scripts/Window/MainPanelManager.cs
+++ Assets/_Seokho/Dark - Complete Horror UI/Scripts/Window/MainPanelManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 namespace Michsky.UI.Dark
 {
@@ -95,169 +97,11 @@ namespace Michsky.UI.Dark
             catch { }
         }
 
-        public void OpenFirstTab()
+
+        public void GameStart()
         {
-            if (currentPanelIndex != 0)
-                OpenPanel(panels[0].panelName);
-
-            else if (currentPanelIndex == 0 && settingsHelper == true && firstTime == false)
-            {
-                OpenPanel(panels[1].panelName);
-                OpenPanel(panels[0].panelName);
-
-                if (panels[0].defaultSelected != null && gamepadEnabled == true)
-                    EventSystem.current.SetSelectedGameObject(panels[0].defaultSelected);
-            }
+            SceneManager.LoadScene("Boot");
         }
 
-        public void OpenPanel(string newPanel)
-        {
-            for (int i = 0; i < panels.Count; i++)
-            {
-                if (panels[i].panelName == newPanel)
-                    newPanelIndex = i;
-            }
-
-            if (newPanelIndex != currentPanelIndex)
-            {
-                StopCoroutine("DisablePreviousPanel");
-
-                currentPanel = panels[currentPanelIndex].panelObject;
-                currentPanelIndex = newPanelIndex;
-                nextPanel = panels[currentPanelIndex].panelObject;
-                nextPanel.SetActive(true);
-
-                currentPanelAnimator = currentPanel.GetComponent<Animator>();
-                nextPanelAnimator = nextPanel.GetComponent<Animator>();
-
-                currentPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                currentPanelAnimator.CrossFade(panelFadeOut, animationSmoothness);
-                nextPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                nextPanelAnimator.CrossFade(panelFadeIn, animationSmoothness);
-
-                StartCoroutine("DisablePreviousPanel");
-
-                if (panels[currentButtonIndex].panelButton != null)
-                    currentButton = panels[currentButtonIndex].panelButton;
-
-                currentButtonIndex = newPanelIndex;
-
-                if (panels[currentButtonIndex].panelButton != null)
-                {
-                    nextButton = panels[currentButtonIndex].panelButton;
-                    currentButtonAnimator = currentButton.GetComponent<Animator>();
-                    nextButtonAnimator = nextButton.GetComponent<Animator>();
-                    currentButtonAnimator.Play(buttonFadeOut);
-                    nextButtonAnimator.Play(buttonFadeIn);
-                }
-
-                if (panels[currentPanelIndex].defaultSelected != null && gamepadEnabled == true)
-                    EventSystem.current.SetSelectedGameObject(panels[currentPanelIndex].defaultSelected);
-            }
-        }
-
-        public void NextPage()
-        {
-            if (currentPanelIndex <= panels.Count - 2)
-            {
-                StopCoroutine("DisablePreviousPanel");
-
-                currentPanel = panels[currentPanelIndex].panelObject;
-
-                if (panels[currentButtonIndex].panelButton != null)
-                    currentButton = panels[currentButtonIndex].panelButton;
-
-                if (panels[currentButtonIndex + 1].panelButton != null)
-                    nextButton = panels[currentButtonIndex + 1].panelButton;
-
-                currentPanel.gameObject.SetActive(true);
-                currentPanelAnimator = currentPanel.GetComponent<Animator>();
-
-                if (currentButton != null)
-                {
-                    currentButtonAnimator = currentButton.GetComponent<Animator>();
-                    currentButtonAnimator.Play(buttonFadeNormal);
-                }
-
-                currentPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                currentPanelAnimator.CrossFade(panelFadeOut, animationSmoothness);
-
-                currentPanelIndex += 1;
-                currentButtonIndex += 1;
-                nextPanel = panels[currentPanelIndex].panelObject;
-                nextPanel.gameObject.SetActive(true);
-
-                nextPanelAnimator = nextPanel.GetComponent<Animator>();
-                nextPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                nextPanelAnimator.CrossFade(panelFadeIn, animationSmoothness);
-
-                if (nextButton != null)
-                {
-                    nextButtonAnimator = nextButton.GetComponent<Animator>();
-                    nextButtonAnimator.Play(buttonFadeIn);
-                }
-
-                if (panels[currentPanelIndex].defaultSelected != null && gamepadEnabled == true)
-                    EventSystem.current.SetSelectedGameObject(panels[currentPanelIndex].defaultSelected);
-            }
-        }
-
-        public void PrevPage()
-        {
-            if (currentPanelIndex >= 1)
-            {
-                StopCoroutine("DisablePreviousPanel");
-
-                currentPanel = panels[currentPanelIndex].panelObject;
-
-                if (panels[currentButtonIndex].panelButton != null)
-                    currentButton = panels[currentButtonIndex].panelButton;
-
-                if (panels[currentButtonIndex - 1].panelButton != null)
-                    nextButton = panels[currentButtonIndex - 1].panelButton;
-          
-                currentPanel.gameObject.SetActive(true);
-                currentPanelAnimator = currentPanel.GetComponent<Animator>();
-
-                if (currentButton != null)
-                {
-                    currentButtonAnimator = currentButton.GetComponent<Animator>();
-                    currentButtonAnimator.Play(buttonFadeNormal);
-                }
-
-                currentPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                currentPanelAnimator.CrossFade(panelFadeOut, animationSmoothness);
-
-                currentPanelIndex -= 1;
-                currentButtonIndex -= 1;
-                nextPanel = panels[currentPanelIndex].panelObject;
-                nextPanel.gameObject.SetActive(true);
-
-                nextPanelAnimator = nextPanel.GetComponent<Animator>();
-                nextPanelAnimator.SetFloat("Anim Speed", animationSpeed);
-                nextPanelAnimator.CrossFade(panelFadeIn, animationSmoothness);
-
-                if (nextButton != null)
-                {
-                    nextButtonAnimator = nextButton.GetComponent<Animator>();
-                    nextButtonAnimator.Play(buttonFadeIn);
-                }
-
-                if (panels[currentPanelIndex].defaultSelected != null && gamepadEnabled == true)
-                    EventSystem.current.SetSelectedGameObject(panels[currentPanelIndex].defaultSelected);
-            }
-        }
-
-        public void AddNewItem()
-        {
-            PanelItem newPanel = new PanelItem();
-            panels.Add(newPanel);
-        }
-
-        IEnumerator DisablePreviousPanel()
-        {
-            yield return new WaitForSeconds(disablePanelAfter);
-            currentPanel.SetActive(false);
-        }
     }
 }
