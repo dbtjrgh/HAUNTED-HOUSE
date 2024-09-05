09d1b41610fcde59177dd44b896f27cd2e77059b
JOB0811 <34418169+JOB0811@users.noreply.github.com>
2024-09-05 14:31:46 +0900

    캠코더 RenderMat이 Quad에 할당이 되고, Debug에서도 확인했으나 아직 Screen에 표기가 되지 않음.

--- Assets/_Wonbin/3. Script/Items/Camcorder.cs
+++ Assets/_Wonbin/3. Script/Items/Camcorder.cs
@@ -13,7 +13,7 @@ namespace Wonbin
         public RenderTexture renderTexture;
         public Material renderTextureMat;
 
-        private bool normalMode = true;
+        private bool normalMode; // 노말 모드인지 고스트 오브 모드인지 확인
         private bool EquipCam; // 카메라 장착 여부
 
         public static bool isInItemSlot;
@@ -23,26 +23,46 @@ namespace Wonbin
         private void Start()
         {
             CamcorderSetup();
+
+            if (screenMesh == null)
+            {
+                Debug.LogError("screenMesh가 할당되지 않았습니다! 확인해주세요.");
+            }
+            else
+            {
+                Debug.Log("screenMesh가 정상적으로 할당되었습니다.");
+            }
             isInItemSlot = false;
             EquipCam = false;
             itemSlotTransform = GameObject.Find("ItemSlot")?.transform;
         }
 
-        public void OnMainUse()    // 사용함수.
+
+        private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            isInItemSlot = transform.IsChildOf(itemSlotTransform);
+
+            if (isInItemSlot)
             {
-                if (itemSlotTransform == null)
+                EquipCam = true;
+                // R 키를 눌렀을 때 모드 전환
+                if (Input.GetKeyDown(KeyCode.R))
                 {
-                    return;
+                    OnMainUse();
                 }
+            }
+            else
+            {
+                EquipCam = false;
+            }
+        }
 
-               if(isInItemSlot)
-                {
-                    EquipCam = true;
-                    Debug.Log("카메라를 장착했습니다.");
-                }
-
+        public void OnMainUse()    
+        {
+            if (EquipCam)  // 캠코더가 장착된 상태에서만 모드 전환 가능
+            {
+                // 초기에는 노말 모드가 호출. 노말 모드에서는 카메라 화면이 보이고, 고스트 오브 모드에서는 초록색 화면이 보임
+                
                 if (normalMode)
                 {
                     SetGhostOrbMode();
@@ -75,7 +95,18 @@ namespace Wonbin
         {
             camcorder.targetTexture = renderTexture;
             greenScreen.targetTexture = renderTexture;
-            screenMesh.material = renderTextureMat;
+            // sharedMaterial을 사용해보세요.
+            screenMesh.sharedMaterial = renderTextureMat;
+
+            // 확인
+            if (screenMesh.sharedMaterial == renderTextureMat)
+            {
+                Debug.Log("screenMesh의 재질(Material)이 renderTextureMat으로 정상적으로 설정되었습니다.");
+            }
+            else
+            {
+                Debug.LogError("screenMesh의 재질(Material)이 renderTextureMat으로 설정되지 않았습니다!");
+            }
         }
     }
 }
